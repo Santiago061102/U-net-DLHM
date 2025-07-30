958657db79ca28a3608f703204f25a312e2ba5ec
Santiago P <santi061102@gmail.com>
2025-07-30 13:50:47 -0500

    DLHM simulation and reconstruction

--- dlhm.py
+++ dlhm.py
@@ -5,6 +5,7 @@ import cv2 as cv
 def dlhm(sample, dx_in, L, z, W_c, dx_out, wavelength, x0=0, y0=0, NA_s=0):
     """
     Digital Lensless Holography Model (DLHM) Simulation.
+    @mariajlopera
 
     Args:
     sample: np.ndarray
@@ -83,7 +84,7 @@ def dlhm(sample, dx_in, L, z, W_c, dx_out, wavelength, x0=0, y0=0, NA_s=0):
         PS = np.exp(-2 * r / (L * np.tan(np.arcsin(NA_s))))
     else:
         PS = np.exp(-r)
-    PS = PS - np.min(PS)
+    # PS = PS - np.min(PS)
     PS = PS / np.max(PS)
 
     # Spatial frequency coordinates at the sample's plane
@@ -116,6 +117,77 @@ def dlhm(sample, dx_in, L, z, W_c, dx_out, wavelength, x0=0, y0=0, NA_s=0):
     holo = np.round(holo).astype(np.uint8)
 
     return holo
+
+
+def dlhm_rec(sample, dx_in, L, z, W_c, dx_out, wavelength, x0=0, y0=0, NA_s=0):
+
+    N, M = sample.shape
+
+    # The discretization on the camera is set as the same input's discretization
+    P = M
+    Q = N
+
+    # Magnification factor
+    Mag = L / z
+    W_s = W_c / Mag  # the size of the window (field of view) that will be propagated
+
+    # Re-sample if desired
+    if dx_in != 0:
+        res_d = dx_in * Mag / dx_out  # ratio of resizing considering the input sample's pixel and the desired
+        # magnification
+        sample = resize(sample, int(M / res_d), int(N / res_d))
+        N_s, M_s = sample.shape
+        W_sample = N_s * dx_out / Mag  # the size of the provided field of view is computed
+
+        if W_sample > W_s:  # if the field of view provided is larger that the field of view that the system captures,
+            # then only the illuminated portion is taken. Else, it is assumed that the provided field
+            # of view is the illuminated portion.
+            sample = sample[N_s // 2 - N // 2:N_s // 2 + N // 2, M_s // 2 - M // 2:M_s // 2 + M // 2]
+        else:
+            sample = resize(sample, P, Q)
+
+        # Crop the sample based on the magnification and offsets
+        start_x = int(N / 2 - N / (Mag * 2) + x0)
+        end_x = int(N / 2 + N / (Mag * 2) + x0)
+        start_y = int(M / 2 - M / (Mag * 2) + y0)
+        end_y = int(M / 2 + M / (Mag * 2) + y0)
+        sample = sample[start_x:end_x, start_y:end_y]
+
+        # Resample to match sensor characteristics
+        sample = resize(sample, P, Q)
+
+    # Wave number
+    k = 2 * np.pi / wavelength
+
+    # Spatial coordinates in the camera's plane
+    x = np.linspace(-W_c / 2, W_c / 2, P)
+    y = np.linspace(-W_c / 2, W_c / 2, Q)
+    u, v = np.meshgrid(x, y)
+
+    # Radial distances for source wavefront
+    r = np.sqrt(u ** 2 + v ** 2 + L ** 2)
+
+    # Determine illumination pattern and normalization
+    if NA_s != 0:
+        PS = np.exp(-2 * r / (L * np.tan(np.arcsin(NA_s))))
+    else:
+        PS = np.exp(-r)
+    PS = PS - np.min(PS)
+    PS = PS / np.max(PS)
+
+    # Spatial frequency coordinates at the sample's plane
+    dfx = Mag / (dx_out * M)
+    dfy = Mag / (dx_out * N)
+    fx, fy = np.meshgrid(np.arange(-M / 2 * dfx, M / 2 * dfx, dfx),
+                         np.arange(-N / 2 * dfy, N / 2 * dfy, dfy))
+
+    # Propagation kernel for the Angular Spectrum Method (ASM)
+    E = np.exp(1j * (L - z) * np.sqrt(k ** 2 - 4 * np.pi ** 2 * (fx ** 2 + fy ** 2)))
+
+    # Compute hologram using inverse Fourier transform
+    Uz = ifts(fts(sample) * E)
+
+    return np.abs(Uz), np.angle(np.conj(Uz))
 
 
 def ifts(A):
@@ -152,7 +224,7 @@ def angular_spectrum(A, Wx, Wy, k, z):
     """
     Propagates field A using angular spectrum method.
     """
-    P, Q = A.shape  # Get size of input field
+    Q, P = A.shape  # Get size of input field
     dfx = 1 / Wx  # Frequency sampling interval in x
     dfy = 1 / Wy  # Frequency sampling interval in y
 
