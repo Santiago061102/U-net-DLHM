28199f207384a8b1f50e29b4f72738d45d21c6d0
Santiago P <santi061102@gmail.com>
2025-07-30 09:57:01 -0500

    Inference code finished

--- inference.py
+++ inference.py
@@ -1,18 +1,25 @@
-import numpy as numpy
-import cv2 as cv
 from network import *
 from utils import *
 from PIL import Image
+import numpy as np
 import matplotlib.pyplot as plt
-import torch
-import os
-import json
+import torch, json, cv2
 
 
 
+'''
+A code to make inference using the U-Net models trained.
+Three images are load, one from train dataset, one from validation and 
+a usaf that have not been seen by the network.
+Also the loss function is visualized which must be saved as json dict, 
+with train and  validation loss.
+'''
+
+# Model and loss data paths
 path_model = "/home/spm061102/Documents/TDG/models/July_22_2025_10_47PM_Hybrid_loss1_cells_unet.pt"
-log_data = "/home/spm061102/Documents/TDG/models/July_22_2025_10_47PM_Hybrid_loss1_cells.json"
+log_data = "/home/spm061102/Documents/TDG/models/July_23_2025_03_19AM_Hybrid_loss2_cells.json"
 
+# Images for inference
 train_holo = "/home/spm061102/Documents/TDG/Dataset/Cancer blood cells/src_ph/src0.png"
 train_ph_gt = "/home/spm061102/Documents/TDG/Dataset/Cancer blood cells/tar_ph/tar0.png"
 
@@ -22,15 +29,19 @@ val_ph_gt = "/home/spm061102/Documents/TDG/Dataset/Cancer blood cells/tar_ph/tar
 usaf_holo = "/home/spm061102/Documents/TDG/Dataset/Cancer blood cells/src_ph/src3400.png"
 usaf_ph_gt = "/home/spm061102/Documents/TDG/Dataset/Cancer blood cells/tar_ph/tar3400.png"
 
+
+# GPU o CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+# U-Net model load, with the same structure of the one trained
 model = Unet(filters=64)
 model.load_state_dict(torch.load(path_model, map_location=torch.device(device), weights_only=True))
 
-
+# Evaluation mode
 model.eval()
 
-
+# Resize tranformation
 transforms = Compose([Resize((256,256)),
                       ToTensor()])
 
@@ -44,12 +55,14 @@ val_ph_gt1 = Image.open(val_ph_gt).convert('L')
 usaf_holo1 = Image.open(usaf_holo).convert('L')
 usaf_ph_gt1 = Image.open(usaf_ph_gt).convert('L')
 
-
+# Transformation apply
 input_train = transforms(train_holo1).unsqueeze(0)
 input_val = transforms(val_holo1).unsqueeze(0)
 input_usaf = transforms(usaf_holo1).unsqueeze(0)
 
 
+
+# Inference 
 with torch.no_grad():
     output_train = model(input_train)
 
@@ -59,10 +72,19 @@ with torch.no_grad():
 with torch.no_grad():
     output_usaf = model(input_usaf)
 
-output_train = output_train[0,0,:,:]
-output_val = output_val[0,0,:,:]
-output_usaf = output_usaf[0,0,:,:]
+# To CPU and numpy, must be rotate
+output_train = output_train[0,0,:,:].cpu().numpy()
+output_train = cv2.rotate(cv2.flip(output_train, 1), cv2.ROTATE_90_COUNTERCLOCKWISE)
 
+
+output_val = output_val[0,0,:,:].cpu().numpy()
+output_val = cv2.rotate(cv2.flip(output_val, 1), cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+output_usaf = output_usaf[0,0,:,:].cpu().numpy()
+output_usaf = cv2.rotate(cv2.flip(output_usaf, 1), cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+
+# Visualization of the inferences
 fig, axes = plt.subplots(nrows=2, ncols=3)
 
 axes[0, 0].imshow(train_ph_gt1, cmap='gray')
@@ -88,21 +110,28 @@ axes[0, 2].axis('off') # Optional: Turn off axes ticks and labels
 axes[1, 2].imshow(output_usaf, cmap='gray')
 axes[1, 2].axis('off') # Optional: Turn off axes ticks and labels
 
-#plt.show()
+plt.show()
 
 
-with open(f'{log_data}', 'r') as file:
-    data = json.load(file)
+# Data loss load
+data = json.load(open(f'{log_data}', 'r'))
+data_train = data["train_loss"]
+data_val = data["val_loss"]
 
 
-y_train = np.array(data["train_loss"])
-x_train = np.shape(y_train)
+# Train loss
+x_train = [key for key, _ in data_train.items()]
+y_train = [value for _, value in data_train.items()]
 
-y_val = data["val_loss"]
-x_val = len(y_val)
+# Validation loss
+x_val = [key for key, _ in data_train.items()]
+y_val = [value for _, value in data_val.items()]
 
 
+plt.plot(x_train,y_train, color='maroon')
+plt.plot(x_val,y_val, color='green')
 
-
-
-print(y_train)
+plt.xlabel('Epochs')
+plt.xticks(np.arange(0, len(x_train)-1, step = (len(x_train))//20))
+plt.ylabel('Loss')
+plt.show()
