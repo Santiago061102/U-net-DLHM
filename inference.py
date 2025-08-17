f314c37be50e4c9e6f5edae3dff967c86fd7c27f
Santiago P <santi061102@gmail.com>
2025-08-16 23:43:52 -0500

    Update

--- inference.py
+++ inference.py
@@ -6,8 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch, json, cv2
 
-
-
 '''
 A code to make inference using the U-Net models trained.
 Three images are load, one from train dataset, one from validation and 
@@ -15,17 +13,17 @@ a usaf that have not been seen by the network.
 Also the loss function is visualized which must be saved as json dict, 
 with train and  validation loss.
 '''
-
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 # Model and loss data paths
-pathModel = r'/home/spm061102/Documents/TDG/models/models/August_07_2025_03_32PM_Comb512_unet.pt'
-logData = r'/home/spm061102/Documents/TDG/models/models/August_07_2025_03_32PM_Comb512.json'
+pathModel = r'/home/spm061102/Documents/TDG/models/models/August_15_2025_12_07AM_Rand_figs1_beta1_unet.pt'
+logData = r'/home/spm061102/Documents/TDG/models/models/August_15_2025_12_07AM_Rand_figs1_beta1.json'
 
 # Images for inference
-pathTrainHolo = r'/home/spm061102/Documents/TDG/Dataset/white blood cells/src/src0.png'
-pathTrainPhGT = r'/home/spm061102/Documents/TDG/Dataset/white blood cells/tar_ph/tar0.png'
+pathTrainHolo = r'/home/spm061102/Documents/TDG/Dataset/random shapes/src/src0.png'
+pathTrainPhGT = r'/home/spm061102/Documents/TDG/Dataset/random shapes/tar_ph/tar_ph0.png'
 
-pathValHolo = r'/home/spm061102/Documents/TDG/Dataset/emojis/src/src300.png'
-pathValPhGT = r'/home/spm061102/Documents/TDG/Dataset/emojis/tar_ph/tar_ph300.png'
+pathValHolo = r'/home/spm061102/Documents/TDG/Dataset/white blood cells/src/src0.png'
+pathValPhGT = r'/home/spm061102/Documents/TDG/Dataset/white blood cells/tar_ph/tar0.png'
 
 pathTestHolo = r'/home/spm061102/Documents/TDG/Dataset/test/src/src0.png'
 pathTestPhGT = r'/home/spm061102/Documents/TDG/Dataset/test/tar_ph/tar_ph0.png'
@@ -33,77 +31,82 @@ pathTestPhGT = r'/home/spm061102/Documents/TDG/Dataset/test/tar_ph/tar_ph0.png'
 
 # GPU o CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+print(device) 
 
 
 # U-Net model load, with the same structure of the one trained
-model = Unet(filters=64)
+model = UNet(filters=64)
 model.load_state_dict(torch.load(pathModel, map_location=torch.device(device), weights_only=True))
 
 # Evaluation mode
 model.eval()
 
-imgSize = 512
+imgSize = 256
 # Resize tranformation
 transforms = Compose([Resize((imgSize, imgSize)),
                       ToTensor()])
 
 
 trainHolo = Image.open(pathTrainHolo).convert('L')
-trainPhGT = cv2.resize(np.array(Image.open(pathTrainPhGT).convert('L')), (imgSize, imgSize))
-trainPhGT = (trainPhGT - np.min(trainPhGT))/(np.max(trainPhGT) - np.min(trainPhGT))
+pathTrainPhGT = Image.open(pathTrainPhGT).convert('L')
 
 valHolo = Image.open(pathValHolo).convert('L')
-valPhGT = cv2.resize(np.array(Image.open(pathValPhGT).convert('L')), (imgSize, imgSize))
-valPhGT = (valPhGT - np.min(valPhGT))/(np.max(valPhGT) - np.min(valPhGT))
+pathValPhGT = Image.open(pathValPhGT).convert('L')
 
 testHolo = Image.open(pathTestHolo).convert('L')
-testPhGT = cv2.resize(np.array(Image.open(pathTestPhGT).convert('L')), (imgSize, imgSize))
-testPhGT = (testPhGT - np.min(testPhGT))/(np.max(testPhGT) - np.min(testPhGT))
+pathTestPhGT = Image.open(pathTestPhGT).convert('L')
 
 # Transformation apply
-input_train = transforms(trainHolo).unsqueeze(0)
-input_val = transforms(valHolo).unsqueeze(0)
-input_test = transforms(testHolo).unsqueeze(0)
+inputTrain = transforms(trainHolo).unsqueeze(0)
+inputVal = transforms(valHolo).unsqueeze(0)
+inputTest = transforms(testHolo).unsqueeze(0)
+
+trainPhGT = transforms(pathTrainPhGT).unsqueeze(0)
+valPhGT = transforms(pathValPhGT).unsqueeze(0)
+testPhGT = transforms(pathTestPhGT).unsqueeze(0)
 
 
 
 # Inference 
 with torch.no_grad():
-    outputTrain = model(input_train)
+    outputTrain = model(inputTrain)
 
 with torch.no_grad():
-    outputVal = model(input_val)
+    outputVal = model(inputVal)
 
 with torch.no_grad():
-    outputTest = model(input_test)
+    outputTest = model(inputTest)
+
+crit = SSIMLoss()
+ssimTrain = crit(trainPhGT, outputTrain)
+ssimVal = crit(valPhGT, outputVal)
+ssimTest = crit(testPhGT, outputTest)
 
 # To CPU and numpy, must be rotate and normalized
 outputTrain = outputTrain[0,0,:,:].cpu().numpy()
 outputTrain = cv2.rotate(cv2.flip(outputTrain, 1), cv2.ROTATE_90_COUNTERCLOCKWISE)
-outputTrain = (outputTrain - np.min(outputTrain))/(np.max(outputTrain) - np.min(outputTrain))
 
 outputVal = outputVal[0,0,:,:].cpu().numpy()
 outputVal = cv2.rotate(cv2.flip(outputVal, 1), cv2.ROTATE_90_COUNTERCLOCKWISE)
-outputVal = (outputVal - np.min(outputVal))/(np.max(outputVal) - np.min(outputVal))
 
 outputTest = outputTest[0,0,:,:].cpu().numpy()
 outputTest = cv2.rotate(cv2.flip(outputTest, 1), cv2.ROTATE_90_COUNTERCLOCKWISE)
-outputTest = (outputTest - np.min(outputTest))/(np.max(outputTest) - np.min(outputTest))
 
 # The MSE of the train, validation and test images are calculated and displayed
-mseTrain = np.mean((trainPhGT - outputTrain)**2)
+trainPhGT = trainPhGT[0,0,:,:].numpy()
+valPhGT = valPhGT[0,0,:,:].numpy()
+testPhGT = testPhGT[0,0,:,:].numpy()
+
+mseTrain = np.mean((trainPhGT- outputTrain)**2)
 mseVal = np.mean((valPhGT - outputVal)**2)
 mseTest = np.mean((testPhGT - outputTest)**2)
 
 
 # The SSIM of the train, validation and test images are calculated and displayed
-(ssimTrain, diff) = ssim(trainPhGT, outputTrain, full=True, data_range=trainPhGT.max() - trainPhGT.min())
-(ssimVal, diff) = ssim(valPhGT, outputVal, full=True, data_range=valPhGT.max() - valPhGT.min())
-(ssimTest, diff) = ssim(testPhGT, outputTest, full=True, data_range=testPhGT.max() - testPhGT.min())
 
-print(f'MSE: Train {mseTrain}, Validation {mseVal}, Test {mseTest}')
-print(f'SSIM: Train {ssimTrain}, Validation {ssimVal}, Test {ssimTest}')
+print(f'MSE: Train {mseTrain:.5f}, Validation {mseVal:5f}, Test {mseTest:5f}')
+print(f'SSIM: Train {ssimTrain:.5f}, Validation {ssimVal:.5f}, Test {ssimTest:.5f}')
+
 
 
 # Visualization of the inferences
